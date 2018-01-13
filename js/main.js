fcecd5e77cf0a9578c658edb7b1f2e4954db3ac4
Surya Kant Bansal <skb1129@yahoo.com>
2018-01-13 11:41:56 +0530

    add service worker for offline use

--- js/main.js
+++ js/main.js
@@ -177,3 +177,23 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+/*
+ * Service Worker Register.
+ */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
