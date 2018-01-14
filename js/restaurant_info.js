7e43b61572aa1bfd77766ba35c335bd321fcba89
Surya Kant Bansal <skb1129@yahoo.com>
2018-01-14 12:03:54 +0530

    nav breadcrumb

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -141,7 +141,7 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.getElementById('breadcrumb').children[0];
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
